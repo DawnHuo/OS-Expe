1fa7d7573e86ffab6e0a01407426c518384c8d7f
dawnHuo <472752575@qq.com>
2018-06-25 18:35:12 +0800

    add ring.c

--- MultiThread/ring.c
+++ MultiThread/ring.c
@@ -18,62 +18,69 @@ TN向T1发送整数N
 #include <stdio.h>
 #include <semaphore.h>
 
-struct index{
+struct index
+{
 	int i;
 };
 int num;
 #define COUNT 10
 
-
 sem_t wait_num[COUNT];
 sem_t mutex;
 pthread_t ring_tid[COUNT];
 
-void *send(void *arg) {
-	
+void *send(void *arg)
+{
+	int i;
+
 	struct index *index;
 	index = (struct index *)arg;
-	
+
 	sem_wait(&wait_num[index->i]);
 	sem_wait(&mutex);
-	
+	printf("T%d accept %d from T%d\n", index->i + 1, num, (index->i) % COUNT);
+
 	num++;
-	
+
 	printf("T%d send %d to T%d\n", index->i + 1, num, (index->i + 2) % COUNT);
-	
+
 	int next = (index->i + 1) % COUNT;
 	sem_post(&mutex);
 	sem_post(&wait_num[next]);
-	
-	
-	if(index->i == 0)
+
+	if (index->i == 0)
 	{
 		int count_join = 1;
-		while(count_join < COUNT){
-			pthread_join(ring_tid[count_join], NULL);
-			count_join++;
-		}
+		pthread_join(ring_tid[COUNT - 1], NULL);
+
+		sem_wait(&wait_num[index->i]);
+		sem_wait(&mutex);
+		printf("T%d accept %d from T%d\n", index->i + 1, num, COUNT);
+		sem_post(&mutex);
+		sem_post(&wait_num[next]);
 	}
-	
+
 	return NULL;
 }
 
-int main() 
+int main()
 {
 	int i;
-	
+
 	struct index indexs[COUNT];
-	
-    sem_init(&mutex, 0,1);
-	for(i = 0; i < COUNT; i++)
+
+	sem_init(&mutex, 0, 1);
+	for (i = 0; i < COUNT; i++)
 	{
 		struct index *index;
 		index = &indexs[i];
 		index->i = i;
-		sem_init(&wait_num[i],0, 0);
+		sem_init(&wait_num[i], 0, 0);
 		pthread_create(&ring_tid[i], NULL, send, index);
 	}
-	
-	sem_post(&wait_num[0]);	
+
+	sem_post(&wait_num[0]);
+
 	pthread_join(ring_tid[0], NULL);
+
 }
